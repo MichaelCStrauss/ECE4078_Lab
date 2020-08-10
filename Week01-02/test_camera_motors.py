1a14663f066da1d814822fdcb1661414968227f2
Michael Strauss <michael.strauss@live.com.au>
2020-08-10 15:46:51 +1000

    Add week 1 code

--- Week01-02/test_camera_motors.py
+++ Week01-02/test_camera_motors.py
@@ -7,10 +7,10 @@ import requests
 import cv2 
 
 def set_velocity(vel0, vel1):
-    r = requests.get("http://localhost:40000/robot/set/velocity?value="+str(vel0)+","+str(vel1))
+    r = requests.get("http://localhost:8080/robot/set/velocity?value="+str(vel0)+","+str(vel1))
 
 def get_image():
-    r = requests.get("http://localhost:40000/camera/get")
+    r = requests.get("http://localhost:8080/camera/get")
     img = cv2.imdecode(np.fromstring(r.content,np.uint8), cv2.IMREAD_COLOR)
 
     return img
